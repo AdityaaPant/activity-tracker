f66454a1bf37c778610c3d0e2f8589685cba2751
Aditya Pant <pantanupam05@gmail.com>
2025-02-18 22:20:49 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,28 @@ import { CardWithForm } from "./brief/page";
 import { Component } from "./graph/page";
 import { ScrollAreaDemo } from "./notes/page";
 export default function Home() {
-  return (
-    <div>
-      <CarouselSize />
-      <div className="flex mx-32 my-20  ">
-        <CalendarDemo />
-        <CardWithForm />
-      </div>
-      <Component />
-      <div className="flex mx-24 my-20 ">
-        <CardWithForm />
-        <div className="ml-10 mx-0 ">
-          <CalendarDemo />
-        </div>
-      </div>
-      <div className="flex mx-28 my-20">
-        <div className="mr-7 ">
-          <ScrollAreaDemo />
-        </div>
+	return (
+		<div>
+			<CarouselSize />
+			<div className="flex mx-32 my-20  ">
+				<CalendarDemo />
+				<CardWithForm />
+			</div>
+			<Component />
+			<div className="flex mx-24 my-20 ">
+				<CardWithForm />
+				<div className="ml-10 mx-0 ">
+					<CalendarDemo />
+				</div>
+			</div>
+			<div className="flex mx-28 my-20">
+				<div className="mr-7 ">
+					<ScrollAreaDemo />
+				</div>
 
-        <CardWithForm />
-      </div>
-      <CarouselSize />
-    </div>
-  );
+				<CardWithForm />
+			</div>
+			<CarouselSize />
+		</div>
+	);
 }
